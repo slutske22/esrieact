4c4430d8c02e6b3865604b4b39018046f4053d04
Seth Lutske <seth.lutske@gmail.com>
2025-07-17 14:26:34 -1000

    Fixing SSR issue!

--- docs/src/theme/ReactLiveScope/index.tsx
+++ docs/src/theme/ReactLiveScope/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import * as Esrireact from "../../../../packages/esrieact/dist";
 import * as constants from "./constants";
 
 // Add react-live imports you need here
@@ -7,11 +6,15 @@ const ReactLiveScope: unknown = {
   React,
   ...React,
 
-  // Enable all esrieact imports in interactive code examples in mdx
-  ...Esrireact,
-
   // constants
   ...constants,
 };
+
+// Only import esrieact on the client side
+if (typeof window !== "undefined") {
+  import("../../../../packages/esrieact/dist").then((Esrireact) => {
+    Object.assign(ReactLiveScope as any, Esrireact);
+  });
+}
 
 export default ReactLiveScope;
